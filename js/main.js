ec39573d82c8c0a9044a67be2f604376848025a9
qmzik <acke208@yandex.ru>
2017-04-08 23:40:19 +0500

    index.html is changed

--- js/main.js
+++ js/main.js
@@ -128,14 +128,71 @@
 	};
 	
 
+    var formTab = function() {
 
+		$('.tab-menu a').on('click', function(event){
+			var $this = $(this),
+				data = $this.data('tab');
+
+			$('.tab-menu li').removeClass('active');
+			$this.closest('li').addClass('active')
+
+			$('.tab .tab-content-inner').removeClass('active');
+			$this.closest('.tab').find('.tab-content-inner[data-content="'+data+'"]').addClass('active');
+
+			event.preventDefault();
+
+		});
+
+	};
+    
+
+    var burgerMenu = function() {
+
+		$('body').on('click', '.js-gtco-nav-toggle', function(event){
+			var $this = $(this);
+
+
+			if ( $('body').hasClass('overflow offcanvas') ) {
+				$('body').removeClass('overflow offcanvas');
+			} else {
+				$('body').addClass('overflow offcanvas');
+			}
+			$this.toggleClass('active');
+			event.preventDefault();
+
+		});
+	};
+
+    var dropdown = function() {
+
+		$('.has-dropdown').mouseenter(function(){
+
+			var $this = $(this);
+			$this
+				.find('.dropdown')
+				.css('display', 'block')
+				.addClass('animated-fast fadeInUpMenu');
+
+		}).mouseleave(function(){
+			var $this = $(this);
+
+			$this
+				.find('.dropdown')
+				.css('display', 'none')
+				.removeClass('animated-fast fadeInUpMenu');
+		});
+
+	};
+    
+    
 	// Document on load.
 	$(function(){
 		fullHeight();
-		burgerMenu();
 		owlCrouselFeatureSlide();
 		contentWayPoint();
 		parallax();
+        dropdown();
 	});
 
 
