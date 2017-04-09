4dafa5b6e9b05dac984a96416f279f2b5043a1ac
qmzik <acke208@yandex.ru>
2017-04-09 14:48:06 +0500

    fixed registration page

--- js/main.js
+++ js/main.js
@@ -185,10 +185,29 @@
 
 	};
     
+    	var formTab = function() {
+
+		$('.tab-menu a').on('click', function(event){
+			var $this = $(this),
+				data = $this.data('tab');
+
+			$('.tab-menu li').removeClass('active');
+			$this.closest('li').addClass('active')
+
+			$('.tab .tab-content-inner').removeClass('active');
+			$this.closest('.tab').find('.tab-content-inner[data-content="'+data+'"]').addClass('active');
+
+			event.preventDefault();
+
+		});
+
+	};
+    
     
 	// Document on load.
 	$(function(){
 		fullHeight();
+        formTab();
 		owlCrouselFeatureSlide();
 		contentWayPoint();
 		parallax();
