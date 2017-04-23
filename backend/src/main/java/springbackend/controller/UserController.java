fec3efcc574c181a598873a6463b6c45fa5472da
Andrey <andreykostyuchenko74@gmail.com>
2017-04-23 19:00:24 +0500

    minor changes

--- backend/src/main/java/springbackend/controller/UserController.java
+++ backend/src/main/java/springbackend/controller/UserController.java
@@ -199,7 +199,6 @@ public class UserController {
         return "registration-confirm";
     }
 
-
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login(Model model, String error, String logout) {
         if (error != null) {
